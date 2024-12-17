55ef82393849138f68e1c281f0f7bc0ea7f11b2e
UrecheanuVladimir <161730825+UrecheanuVladimir@users.noreply.github.com>
2024-12-17 13:54:15 +0200

    Add files via upload

---
+++ UrecheanuVladimir/BucketSort.py
@@ -0,0 +1,30 @@
+def insertion_sort(bucket):
+    for i in range(1, len(bucket)):
+        key = bucket[i]
+        j = i - 1
+        while j >= 0 and bucket[j] > key:
+            bucket[j + 1] = bucket[j]
+            j -= 1
+        bucket[j + 1] = key
+
+def bucket_sort(arr):
+    n = len(arr)
+    buckets = [[] for _ in range(n)]
+
+    for num in arr:
+        bi = int(n * num)
+        buckets[bi].append(num)
+
+    for bucket in buckets:
+        insertion_sort(bucket)
+
+    index = 0
+    for bucket in buckets:
+        for num in bucket:
+            arr[index] = num
+            index += 1
+
+arr = list(map(float, input("Unsorted array: ").split()))
+bucket_sort(arr)
+print("Sorted array is:")
+print(" ".join(map(str, arr)))
