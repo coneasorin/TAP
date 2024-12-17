fa4894947d284cefbc39a7216c1543e863e7f1f2
StefanAndreiO <162457485+StefanAndreiO@users.noreply.github.com>
2024-12-17 13:54:48 +0200

    Add files via upload

---
+++ Ohescu Stefan/Binary_search.py
@@ -0,0 +1,27 @@
+def binarySearch(array, x, low, high):
+
+    while low <= high:
+
+        mid = low + (high - low)//2
+
+        if x == array[mid]:
+            return mid
+
+        elif x > array[mid]:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Elementul este prezent " + str(result))
+else:
+    print("Nu a fost gasit")
