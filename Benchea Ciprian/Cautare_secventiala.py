bcd6f475777766b61b4375a7adea5e2ddae4b508
BDCiprian <benchea.ciprian@yahoo.com>
2024-12-18 14:17:00 +0200

    Implementari

---
+++ Benchea Ciprian/Cautare_secventiala.py
@@ -0,0 +1,26 @@
+def cautare_secventiala(arr, cautat):
+     low = 1
+     high = len(arr) - 1
+     poz = 0
+
+     while (low <= high) and (poz == 0):
+        
+         if arr(low) == cautat:
+             poz = low
+             return poz
+         else:
+             low +=1
+     return -1
+
+with open(r"C:\Users\Student\Desktop\BDC Tap\TAP\Benchea Ciprian\sir_cautare.txt", 'r') as fisier:
+    randuri_fisier = fisier.readlines()
+    sir = list(map(int, randuri_fisier[0].strip().split())) #folosim split() pentru a delimita numerele separate prin spatiu
+
+sir = sorted(sir)
+print (f"sirul sortat este {sir}")
+cautat = int(input("Introdu numarul cautat : "))
+result = cautare_secventiala(sir, cautat)
+if result != -1:
+    print (f"Elementul {cautat} a fost gasit la indexul {result + 1}.")
+else:
+    print(f"Elementul {cautat} nu a fost gasit.")
