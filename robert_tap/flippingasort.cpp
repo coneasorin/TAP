2cd56768f2fac5323e85691c1082a0272db4d302
robertgiv <162297788+robertgiv@users.noreply.github.com>
2025-01-15 11:16:03 +0200

    Add files via upload

---
+++ robert_tap/flippingasort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void flip(vector<int>& arr, int i) {
+    reverse(arr.begin(), arr.begin() + i + 1);
+}
+
+void pancakeSort(vector<int>& arr) {
+    for (int curr_size = arr.size(); curr_size > 1; curr_size--) {
+        int max_idx = max_element(arr.begin(), arr.begin() + curr_size) - arr.begin();
+        if (max_idx != curr_size - 1) {
+            flip(arr, max_idx);
+            flip(arr, curr_size - 1);
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {3, 6, 2, 7, 5, 8, 1};
+    pancakeSort(arr);
+    for (int num : arr) 
+        cout << num << " ";
+    return 0;
+}
